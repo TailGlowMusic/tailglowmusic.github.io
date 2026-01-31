240766661bea772e9bf32b2ab17c3e674f430b36
Wyatt Strother <tailglowmusic@gmail.com>
2026-01-31 11:34:16 -0500

    videos updated

--- src/routes/videos/+page.svelte
+++ src/routes/videos/+page.svelte
@@ -1,6 +1,26 @@
 
 <div class="card flex flex-col items-center preset-glass-surface p-5 my-5 w-4/5">
     <div>
+        <h3 class="h3">Food Around the Corner - Soul Bar 2024</h3>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Vr5WNi2s-qY?si=UmWgjHpvBKjbak5E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+
+    <div>
+        <h3 class="h3">Dying in West Virginia - Soul Bar 2024</h3>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/I1xopyCTL3M?si=OwjJkYELQMjXUeF1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+
+    <div>
+        <h3 class="h3">DuX AmuX ReduX - Soul Bar 2024</h3>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/8FMey_qE5hg?si=bW2mIe82x140315-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+
+    <div>
+        <h3 class="h3">Snails 'n' Paper - Soul Bar 2024</h3>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/0i3k0kWZdbs?si=PRMIvthH7-9zht6j" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+
+    <div>
         <h3 class="h3">Food Around the Corner</h3>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Ob5RaZSzWLg?si=Du9OtEgw5QCH8oGj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
